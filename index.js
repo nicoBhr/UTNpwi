b1f9b5e30f041d4801b3660e4b82ef1aa1cfdcaf
nicoBhr <nicolasbecher@hotmail.com>
2022-01-21 22:55:36 -0300

    areglo

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+const express = require("express")
 
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log(`Servidor corriendo en el puerto 3001`); 
   });
 });
+
+server.use(express.static("./public"))
