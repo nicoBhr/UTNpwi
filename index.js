35899bf669feb9d3424f4f8a9e3c770d971e5b38
nicoBhr <nicolasbecher@hotmail.com>
2022-01-25 20:23:26 -0300

    nuevo

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const express = require("express")
+const express = require("express");
+require("dotenv").config();
 
 conn.sync({ force: false }).then(() => {
-  server.listen(5432, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log(`Servidor corriendo en el puerto 3001`); 
   });
 });
